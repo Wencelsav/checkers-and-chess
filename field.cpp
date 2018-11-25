52c79dac07a64cf644f0192796eceb1cfea84691
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2018-11-26 02:46:40 +0300

    Update field.cpp

--- field.cpp
+++ field.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
-#include <stdio.h>
+#include <windows.h>
+
 using namespace std;
 
-int main() {
-	int f[8][8];
+int main(void)
+{
+	int a[8][8];
+	//ввод матрицы
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++) {
-		    int n=i+j;
-		    if (n%2==0){
-		        f[i][j]=0;
-		    }
-			cout << f[i][j]<<endl;
+			a[i][j] = 0;
+			int n = i + j;
+			if (n % 2 == 0) {
+				SetConsoleTextAttribute(console, BACKGROUND_BLUE); //печатаем зеленым
+				cout << a[i][j];
+				SetConsoleTextAttribute(console, BACKGROUND_GREEN); //печатаем белым
+			}
+			cout << a[i][j];
 		}
+		
 	}
-
-
-
+	system("pause");
+	return 0;
 }
