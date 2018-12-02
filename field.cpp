1dd1ce59b291c8d752e1e09f42555d695cfdfe8b
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2018-12-02 10:20:08 +0300

    Update field.cpp

--- field.cpp
+++ field.cpp
@@ -1,26 +1,71 @@
-#include <iostream>
+#include <iostream> 
 #include <windows.h>
-
 using namespace std;
-
-int main(void)
+enum class Color
 {
-	int a[8][8];
-	//ввод матрицы
-	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			a[i][j] = 0;
-			int n = i + j;
-			if (n % 2 == 0) {
-				SetConsoleTextAttribute(console, BACKGROUND_BLUE); //печатаем зеленым
-				cout << a[i][j];
-				SetConsoleTextAttribute(console, BACKGROUND_GREEN); //печатаем белым
-			}
-			cout << a[i][j];
-		}
-		
+	BLACK = 0,
+	WHITE = 1
+};
+struct Cell
+{
+	int x;
+	int y;
+	Color color;
+};
+class Desk
+{
+private:
+	HWND m_ConsoleWindow;
+	HDC m_hDC;
+	int m_width;
+	int m_height;
+	int m_leftX;
+	int m_leftY;
+	int m_cellSize;
+	Cell** m_cellArray;
+public:
+	Desk() :
+		m_ConsoleWindow(GetConsoleWindow()),
+		m_hDC(GetDC(m_ConsoleWindow)),
+		m_width(8),
+		m_height(8),
+		m_leftX(15),
+		m_leftY(15),
+		m_cellArray(nullptr),
+		m_cellSize(30)
+	{
+		m_cellArray = new Cell*[m_height];
+		for (int i = 0; i < m_width; i++)
+			m_cellArray[i] = new Cell;
+
 	}
+	~Desk()
+	{
+		for (int i = 0; i < m_width; i++)
+			delete[] m_cellArray[i];
+		delete[] m_cellArray;
+	}
+
+	void showDesk()
+	{
+		for (int i = 0; i < m_height; i++)
+		{
+			for (int q = 0; q < m_width; q++)
+			{
+				if ((q + i) % 2 == 0)
+					SelectObject(m_hDC, CreateSolidBrush(RGB(77, 75, 34)));
+				else
+					SelectObject(m_hDC, CreateSolidBrush(RGB(81, 13, 22)));
+				Rectangle(m_hDC, m_leftX + q * m_cellSize, m_leftY + i * m_cellSize, m_leftX + q * m_cellSize + m_cellSize, m_leftY + i * m_cellSize + m_cellSize);
+				// написал struct Cell и enum Color для расширяемости, но я их вообще не использовал по сути) 
+			}
+		}
+
+	}
+};
+int main()
+{
+	Desk hello;
+	hello.showDesk();
 	system("pause");
-	return 0;
 }
